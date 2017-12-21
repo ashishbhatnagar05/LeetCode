21b22b41ea1699cf49d7d59eb84616cd2b23cc25
kaal-PC\kaal <ashishbhatnagar05@gmail.com>
2017-12-22 00:54:12 +0530

    Leetcode 647. Palindromic Substrings

--- src/string1/StringQuestions1.java
+++ src/string1/StringQuestions1.java
@@ -1,17 +1,13 @@
 package string1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringQuestions1 {
 
-	public String reverseStringUsingStringBuilder(String s) {
-
-		StringBuilder sb = new StringBuilder();
-
-		char[] charArray = s.toCharArray();
-		int n = charArray.length;
-		for (int i = n - 1; i >= 0; i--) {
-			sb.append(charArray[i]);
-		}
-		return sb.toString();
+	public String reverseStringUsingStringBuilder(String str) {
+		StringBuilder sb = new StringBuilder(str);
+		return sb.reverse().toString();
 	}
 
 	public String reverseStringUsingJustArrays(String s) {
@@ -22,5 +18,92 @@ public class StringQuestions1 {
 			reverseArray[j] = charArray[i];
 		}
 		return new String(reverseArray);
+	}
+
+	public int countPalindromeSubstrings(String str) {
+		String[] substrings = getSubStrings(str);
+		int count = 0;
+		for (String subStr : substrings) {
+			if (isPalindrome(subStr)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public String[] getSubStrings(String str) {
+		int strLength = str.length();
+		int resultArrayLength = 0;
+		for (int i = 0; i <= strLength; i++) {
+			resultArrayLength = resultArrayLength + i;
+		}
+		String[] resultArray = new String[resultArrayLength];
+		int n = str.length();
+		int count = 0;
+		for (int i = 0; i <= n; i++) {
+			for (int j = i + 1; j <= n; j++) {
+				resultArray[count] = str.substring(i, j);
+				count++;
+			}
+		}
+		return resultArray;
+	}
+
+	public boolean isPalindrome(String str) {
+		char[] charArray = str.toCharArray();
+		char[] reverseArray = new char[charArray.length];
+		int n = charArray.length;
+		for (int i = n - 1, j = 0; i >= 0; i--, j++) {
+			reverseArray[j] = charArray[i];
+		}
+		String reverseString = new String(reverseArray);
+		if (reverseString.equals(str))
+			return true;
+		return false;
+	}
+
+	public int countPalindromeSubstringsUsingList(String str) {
+		List<String> substrings = getSubStringsUsingList(str);
+		int count = 0;
+		for (String subStr : substrings) {
+			if (isPalindromeUsingStringBuilder(subStr)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public List<String> getSubStringsUsingList(String str) {
+		List<String> resultList = new ArrayList<String>();
+		int n = str.length();
+		for (int i = 0; i <= n; i++) {
+			for (int j = i + 1; j <= n; j++) {
+				resultList.add(str.substring(i, j));
+			}
+		}
+		return resultList;
+	}
+
+	public int countPalindromeSubstringsUsingMap(String str) {
+		int n = str.length();
+		int count = 0;
+		for (int i = 0; i <= n; i++) {
+			for (int j = i + 1; j <= n; j++) {
+				String s = str.substring(i, j);
+				boolean ispalindrome = isPalindromeUsingStringBuilder(s);
+				if (ispalindrome) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	public boolean isPalindromeUsingStringBuilder(String str) {
+		StringBuilder sb = new StringBuilder(str);
+		sb.reverse();
+		if (sb.toString().equals(str))
+			return true;
+		return false;
 	}
 }
--- src/string1Test/StringQuestions1Test.java
+++ src/string1Test/StringQuestions1Test.java
@@ -1,6 +1,9 @@
 package string1Test;
 
+import java.util.Arrays;
+
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import string1.StringQuestions1;
@@ -13,15 +16,37 @@ public class StringQuestions1Test {
 		stringQuestions1 = new StringQuestions1();
 	}
 
+	@Ignore
 	@Test
 	public void reverseStringUsingStringBuilder() {
 		String str = stringQuestions1.reverseStringUsingJustArrays("Hello");
 		System.out.println(str);
 	}
 
+	@Ignore
 	@Test
 	public void reverseStringUsingJustArraysTest() {
 		String str = stringQuestions1.reverseStringUsingJustArrays("Hello");
 		System.out.println(str);
+	}
+
+	@Ignore
+	@Test
+	public void isPalindromeTest() {
+		boolean isPalindrome = stringQuestions1.isPalindrome("aba");
+		System.out.println(isPalindrome);
+	}
+
+	@Ignore
+	@Test
+	public void getSubStringsTest() {
+		String[] stringArray = stringQuestions1.getSubStrings("abcdef");
+		System.out.println(Arrays.asList(stringArray));
+	}
+	//@Ignore
+	@Test
+	public void countPalindromeSubstringsTest() {
+		int count = stringQuestions1.countPalindromeSubstringsUsingMap("aaa");
+		System.out.println(count);
 	}
 }
